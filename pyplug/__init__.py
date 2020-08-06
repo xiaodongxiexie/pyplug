d3e6c898e9c5e981fa19cc15f7eaa9b014a9314d
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2020-08-06 18:19:24 +0800

    Create __init__.py

---
+++ pyplug/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: xiaodong
+# @Date  : 2020/8/6
+
+
+__title__ = 'pyplug'
+__description__ = 'A simple plug system.'
+__version__ = '0.0.1'
+__author__ = 'xiaodong'
+__author_email__ = 'xiaodongliang@outlook.com'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2020 xiaodong'
